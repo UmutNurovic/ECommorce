ad0323c3a38e18ad609b861940be001914215c00
Umut can Aktürk <akturkumutcan58@gmail.com>
2021-09-16 00:02:10 +0300

    Commerce Products

--- client/src/components/Products/Product/Product.jsx
+++ client/src/components/Products/Product/Product.jsx
@@ -7,29 +7,31 @@ import useStyles from './styles';
 
 const Product = ({product}) => {
     const classes = useStyles();
+    
+    console.log(product);
+    
     return (
-        <Card className ={classes.root}>
-            <CardMedia className={classes.media}  image={product.image} title={product.name}/>
-            <CardContent>
-                <div className={classes.cardContent}>
-                    <Typography variant ="h5" gutterBottom>
-                        {product.name}
-
-                    </Typography>
-                    <Typography variant ="h5" >
-                        {product.price}
-
-                    </Typography>
-                </div>
-                <Typography variant="body2" color="textSecondary">{product.description}</Typography>
-            </CardContent>
-            <CardActions disableSpacing className={classes.cardActions}>
-                <IconButton aria-label ="Add To Cart">
-                    <AddShoppingCart/>
-                </IconButton>
-            </CardActions>
+        <Card className={classes.root}>
+          <CardMedia className={classes.media} image={product.media.source} title={product.name} />
+          <CardContent>
+            <div className={classes.cardContent}>
+              <Typography gutterBottom variant="h5" component="h2">
+                {product.name}
+              </Typography>
+              <Typography gutterBottom variant="h5" component="h2">
+                ${product.price.formatted}
+              </Typography>
+            </div>
+            <Typography dangerouslySetInnerHTML={{ __html: product.description }} variant="body2" color="textSecondary" component="p" />
+          </CardContent>
+          <CardActions disableSpacing className={classes.cardActions}>
+            <IconButton aria-label="Add to Cart" >
+              <AddShoppingCart />
+            </IconButton>
+          </CardActions>
         </Card>
-    );
+      );
+    
 }
 
 export default Product;
--- client/src/components/Products/Products.jsx
+++ client/src/components/Products/Products.jsx
@@ -2,17 +2,15 @@ import React from 'react';
 import {Grid} from '@material-ui/core';
 import Product from './Product/Product';
 import useStyles from './style';
-const products = [
-    {id:1,name:'shoes',description:'running soes' ,price:'$5', image:"https://img.rawpixel.com/s3fs-private/rawpixel_images/website_content/rm208-251-jj-58-k_1.jpg?w=800&dpr=1&fit=default&crop=default&q=65&vib=3&con=3&usm=15&bg=F4F4F3&ixlib=js-2.2.1&s=f7264d6da3df4c96180fbe87d0850595"},
-    {id:2,name:'Macbook',description:'Apple macbook ',price:'$10' ,image:"https://www.apple.com/v/macbook-air/k/images/meta/macbook-air_overview__15sjf4iagj6q_og.png?202108090535"},
-];
 
-const Products = ()=>{
+const Products = ({ products })=>{
     const classes = useStyles();
+    if (!products.length) return <p>Loading...</p>;
+
     return (
-        <main className={classes.content}>
+      <main className={classes.content}>
         <div className={classes.toolbar} />
-        <Grid container justify="center" spacing={4}>
+        <Grid container justifyContent="center" spacing={4}>
           {products.map((product) => (
             <Grid key={product.id} item xs={12} sm={6} md={4} lg={3}>
               <Product product={product} />
